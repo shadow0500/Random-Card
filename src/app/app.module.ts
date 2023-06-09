4c89ea51f7abe4349015216df2d5bc26878e4055
Shadmaan <shadmaank2601@gmail.com>
2023-06-09 20:12:08 +0530

    Push after update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome"
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule, 
+    ToastrModule.forRoot(), 
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
